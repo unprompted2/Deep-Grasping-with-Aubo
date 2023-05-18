3eacfd7bced60374a6140b798c8d39e0e20a4c4e
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2023-05-18 09:39:18 +0000

    Updates CHANGELOG

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,16 @@
-import gl
+import glob, os
+
+#Current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+print(current_dir)
+
+current_dir = '/home/nicholasward2/darknet/build/darknet/x64/data/obj'
+
+# Per of images to be used for the test set
+
+percentage_test = 10
+
+#Create and/or truncate train.txt and test.txt
+
+file_train = open('/
